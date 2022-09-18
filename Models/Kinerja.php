6b31d72adb1cf48ca91a0da6794a3029d3dba699
afifikoh <113290533+afifikoh@users.noreply.github.com>
2022-09-18 19:59:21 +0700

    Add files via upload

--- Models/Kinerja.php
+++ Models/Kinerja.php
@@ -5,16 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bidang extends Model
+class Kinerja extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'kd_dinas',
-        'kd_bidang',
-        'bidang'
+        'tgl',
+        'foto',
+        'doc',
+        'hasil'
     ];
-
-
-    protected $table = 'bidang';
+    protected $table = 'kinerja';
 }
