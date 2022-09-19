0a5ec3220c88fd77bdb821fc140a187c862ea896
afifikoh <113290533+afifikoh@users.noreply.github.com>
2022-09-19 07:43:38 +0700

    Add files via upload

--- views/data_bidang/tambah_bdg.blade.php
+++ views/data_bidang/tambah_bdg.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Tambah Data Pegawai</h1>
+                <h1>Tambah Data Bidang</h1>
             </div>    
         </div>
     </div>
