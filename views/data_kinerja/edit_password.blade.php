43d84658490886a9931faac72411cd9946704292
Hardiana <87222054+hardiana4@users.noreply.github.com>
2022-09-24 11:07:57 +0700

    Update edit_password.blade.php
    Signed-off-by: Hardiana <87222054+hardiana4@users.noreply.github.com>

--- views/data_kinerja/edit_password.blade.php
+++ views/data_kinerja/edit_password.blade.php
@@ -19,19 +19,54 @@
     <div class="col-sm-12">
   <div class="card" style="width: 25rem;">
       <div class="card-header">
-          <form>
+          <form action="/update/password/pegawai/{{ $user->id }}" method="POST">
+            @csrf
               <div class="form-group">
                   <div class="col-lg-15">
-                      <label for="exampleFormControlInput1">Password baru</label>
-                      <input type="nama" class="form-control" id="exampleFormControlInput1" placeholder="Password baru ..">
+                      <label for="password">Password Lama</label>
+                      <input type="password" name="password_lama" id="password_lama" class="form-control
+                      @error('password_lama')
+                      is-invalid
+                      @enderror
+                      " placeholder="Masukan Password Lama">
+                      @error('password_lama')
+                          <div class='invalid-feedback'>
+                              {{ $message }}
+                          </div>
+                      @enderror
+                  </div>
+                  </div>
+              <div class="form-group">
+                  <div class="col-lg-15">
+                      <label for="exampleFormControlInput1">Password Baru</label>
+                      <input type="password" class="form-control
+                      @error('password_baru')
+                      is-invalid
+                      @enderror"
+                      name="password_baru" id="password_baru" placeholder="Masukan Password baru ..">
+                      @error('password_baru')
+                          <div class='invalid-feedback'>
+                              {{ $message }}
+                          </div>
+                      @enderror
                   </div>
               </div>
               <div class="form-group">
                   <div class="col-lg-15">
-                      <label for="exampleFormControlInput1">Password baru</label>
-                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ulangi Password baru ..">
+                      <label for="exampleFormControlInput1">Konfirmasi Password</label>
+                      <input type="password" class="form-control
+                      @error('password_konfirmasi')
+                      is-invalid
+                      @enderror"
+                      name="password_konfirmasi" id="password_konfirmasi" placeholder="Ulangi Password baru ..">
+                      @error('password_konfirmasi')
+                          <div class='invalid-feedback'>
+                              {{ $message }}
+                          </div>
+                      @enderror
                   </div><br>
-                  <button type="button" class="btn btn-primary">Perbarui</button>
+                  <button type="submit" class="btn btn-primary">Perbarui</button>
+                  {{-- <button type="cancel" class="btn btn-warning">Batal</button> --}}
                   <a href="{{url('pengaturan-pegawai')}}" class="btn btn-warning">Batal</a>
               </div>
           </form>
@@ -41,3 +76,4 @@
   </div>
   
   @endsection
+  
