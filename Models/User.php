a276c92ca3f11f78232b6ab805ec4a470f14e7e9
afifikoh <113290533+afifikoh@users.noreply.github.com>
2022-09-26 13:46:45 +0700

    Update User.php
    Signed-off-by: afifikoh <113290533+afifikoh@users.noreply.github.com>

--- Models/User.php
+++ Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     protected $casts = [
         "email_verified_at" => "datetime",
     ];
+    
+    public function nmbidang()
+    {
+        return $this->belongsTo(Bidang::class, 'bidang','id');
+    }
 }
