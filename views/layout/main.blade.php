87f8972afed6a9f9689066ec87199e5e14c71bb6
afifikoh <113290533+afifikoh@users.noreply.github.com>
2022-09-28 07:25:21 +0700

    Update main.blade.php
    Signed-off-by: afifikoh <113290533+afifikoh@users.noreply.github.com>

--- views/layout/main.blade.php
+++ views/layout/main.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="{{ url('http://127.0.0.1:8000/img/logo1.ico') }}">
   <title>Spiner</title>
-
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
@@ -57,7 +57,8 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('/') }}dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          {{-- <img src="{{ asset('/') }}dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> --}}
+          <img src="{{ asset ('img-user/'.$user->foto) }}" alt="user-image" class="img-circle elevation-2">
         </div>
         <div class="info">
           
@@ -130,28 +131,29 @@
 <script src="{{ asset('/') }}plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
 <script src="{{ asset('/') }}plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
 
- 
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script> --}}
 
-{{-- <!-- AdminLTE for demo purposes -->
-<script src="{{ asset('/') }}dist/js/demo.js"></script> --}}
-{{-- <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-</script> --}}
-@stack('main-script')
+<script>
+  $('#dt_table').dataTable();
+  
+  $('#bd_pegawai').on('click','.detail',function(){
+    let id = $(this).data('id');
+    $.ajax({
+      url :'get-pegawai',
+      data:{a:id},
+      type:'GET',
+      dataType:'JSON',
+      success:function(){
+        $('#modal-pegawai').modal('show');
+
+      }
+    })
+  })
+</script>
 
 </body>
 </html>
+
