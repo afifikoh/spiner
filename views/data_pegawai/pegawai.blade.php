ffb79a35e98fe93ab1cb4cb6ef75e81b525354b4
afifikoh <113290533+afifikoh@users.noreply.github.com>
2022-10-05 08:44:29 +0700

    Update pegawai.blade.php
    Signed-off-by: afifikoh <113290533+afifikoh@users.noreply.github.com>

--- views/data_pegawai/pegawai.blade.php
+++ views/data_pegawai/pegawai.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <a href="{{ url('tambah-pegawai') }}" class="nav-link">
-                    <button type="button" class="btn btn-block btn-primary btn-sm"><i class="fa fa-plus-circle"></i> Tambah Data </button>
+                    <button type="button" class="btn btn-block btn-primary"><i class="fa fa-plus-circle"></i> Tambah Data </button>
                     </a>
                 </ol>
                 </div>
@@ -28,21 +28,11 @@
                             <div class="card-header">
                                 <div class="row">
                                     <div class="col-sm-12 col-md-9"></div>
-                                    {{-- <div class="col-sm-12 col-md-3">
-                                        <form action="" method="get">
-                                            <div class="col-sm-3">
-                                              <div class="input-group mb-3" style="width: 240px;">
-                                                <input type="text" name="keyword" class="form-control " placeholder="Search">
-                                                <button type="submit" class="input-group-text btn btn-default "><i class="fas fa-search"></i></button>
-                                              </div>
-                                            </div>
-                                          </form>
-                                    </div> --}}
                                 </div>
 
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <table id="pegawai" class="table table-bordered table-hover dataTable dtr-inline collapsed" >
+                                    <table id="dt_table" class="table table-bordered table-hover dataTable dtr-inline collapsed" >
                                 <thead>
                                     <tr role="row">
                                         <th>No</th>
@@ -56,11 +46,11 @@
                                         <th>Aksi</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody id="bd_pegawai">
                                     @php $no=1; @endphp
                                     @foreach ($users as $data) 
                                     <tr class="odd">
-                                        {{-- <td><img src="{{ asset ('asset/img/pegawai/'.$data['foto']) }}" alt="" class="img-fluid"></td> --}}
+                                        
                                         <td>{{ $no++ }}</td>
                                         <td>{{ $data['nip'] }}</td>
                                         <td>{{ $data['nama'] }}</td>
@@ -68,7 +58,8 @@
                                         <td>{{ $data['email'] }}</td>
                                         <td>{{ $data['no_hp'] }}</td>
                                         <td>{{ $data['level']}}</td>
-                                        <td>{{ $data->nmbidang['bidang'] }}</td> 
+                                        <td>{{ $data->nmbidang->bidang }}</td> 
+                                        {{-- <td><img src="{{ asset ('img-user/'.$data['foto']) }}" alt="user-image" class="img-fluid"></td> --}}
                                         <td>
                                         <div class="d-grid gap-2 d-md-block" style="text-align:center">
                                             <a href="edit-pegawai/{{ $data->id }}">
@@ -77,6 +68,14 @@
                                         <a href="hapus-pegawai/{{ $data->id }}">
                                             <button class="btn btn-danger" type="button"><i class="fa fa-trash"></i></button>
                                         </a>
+                                        <a href="get-pegawai/{{ $data->id }}">
+                                            <button class="btn btn-primary" type="button"><i class="fa fa-eye"></i></button>
+                                        </a>
+
+
+                                        {{-- benar --}}
+                                        {{-- <button class="btn btn-primary detail" type="button"  data-id="{{ $data->id; }}"><i class="fa fa-eye"></i></button> --}}
+                                        
                                         </div>
                                         </td>
                                     </tr>
@@ -93,10 +92,6 @@
             </div>
         </div>
     </section>
-    @push('main-script')
-  <script>
-    $('#pegawai').dataTable();
-  </script>
-@endpush
+    <div class="modal-dialog modal-lg" id="modal-pegawai">...</div>
     
 @endsection
