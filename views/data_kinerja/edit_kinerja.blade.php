f8c7b02d223c71e4d325cc223d5be4665a52cdb1
adeliajihann <102372659+adeliajihann@users.noreply.github.com>
2022-10-07 17:07:14 +0700

    Add files via upload
    Signed-off-by: adeliajihann <102372659+adeliajihann@users.noreply.github.com>

---
+++ views/data_kinerja/edit_kinerja.blade.php
@@ -0,0 +1,84 @@
+@extends('layout.main')
+
+@section('judul')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Edit Data Kinerja</h1>
+            </div>    
+        </div>
+    </div>
+</section>
+@endsection
+
+@section('isi')
+<section class="content">
+  <div class="container-fluid">
+    <div class="col-sm-12">
+      <div class="card">
+              <form action="update/draft/{{ $kinerja->id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="card-body">
+                  {{-- @method('PUT') --}}
+                  <div class="form-group col-md-2">
+                    <label for="tgl">Tanggal</label>
+                    <input type="text" class="form-control" name="tgl" id="tgl" readonly value="{{ date("d/m/Y") }}" data-target="#tgl" data-toggle="datetimepicker">
+                  </div>
+                  <div class="form-group col-md-5">
+                    <label for="hasil">Rincian Kinerja</label>
+                      <input type="text" class="form-control
+                      @error('hasil')
+                        is-invalid
+                      @enderror
+                        " id="hasil" name='hasil' rows="4" value="{{ $kinerja->hasil }}">
+                      @error('hasil')
+                        <div class='invalid-feedback'>
+                          {{ $message }}
+                        </div>
+                      @enderror
+                  </div>
+                  <div class="form-group col-md-5">
+                    <label for="foto">Bukti Foto</label><br>
+                    <a href="{{ asset('template/dist/img/kinerja/'.$kinerja['foto']) }}">Lihat Foto</a>
+                    <div class="input-group">
+                      <input type="file" class="form-control 
+                      @error('foto')
+                        is-invalid
+                      @enderror
+                        " id="foto" name="foto" value="{{ $kinerja->foto }}">
+                      @error('foto')
+                        <div class='invalid-feedback'>
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="form-group col-md-5">
+                    <label for="foto">Bukti Documment .pdf</label><br>
+                    <a href="{{ asset('template/dist/img/kinerja/'.$kinerja['doc']) }}">Lihat Doc</a>
+                    <div class="input-group">
+                      <input type="file" class="form-control 
+                      @error('doc')
+                        is-invalid
+                      @enderror
+                        " id="doc" name="doc" value="{{ $kinerja->doc }}">
+                      @error('doc')
+                        <div class='invalid-feedback'>
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+                </div>
+
+                <div class="float-right">
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                  <a href="{{url('kinerja-pegawai')}}"><i class="btn btn-light">Batal</i></a> 
+                </div>
+              </form>
+      </div>
+    </div>
+  </div>
+</section>
+ @endsection
