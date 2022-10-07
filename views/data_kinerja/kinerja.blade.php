758acec69bd9598c8efd06b6112dd30f4180c51c
adeliajihann <102372659+adeliajihann@users.noreply.github.com>
2022-10-07 17:08:21 +0700

    Update kinerja.blade.php
    Signed-off-by: adeliajihann <102372659+adeliajihann@users.noreply.github.com>

--- views/data_kinerja/kinerja.blade.php
+++ views/data_kinerja/kinerja.blade.php
@@ -7,7 +7,7 @@
     <div class="row mb-4">
         <h1 class="md-0">Data Kinerja Pegawai</h1>
         <div class="col box-header text-right">
-            <a href="{{url('/draft')}}" class="btn btn-rounded btn-warning" style="border-radius:30px;">Draft</a>
+            {{-- <a href="{{url('/draft')}}" class="btn btn-rounded btn-warning" style="border-radius:30px;">Draft</a> --}}
             <a href="{{url('/tambah-kinerja')}}" class="btn btn-primary" style="border-radius:30px;"><i class="fa fa-plus-circle" ></i &nbsp> Tambah Kinerja</a>
       </div><!-- /.col -->
     </div><!-- /.row -->
@@ -21,14 +21,6 @@
 <!-- /.content-header -->
 <div class="container-fluid">
 <div class="card">
-    <!-- /.card-header -->
-    {{-- <div class="card-header">
-      <div class="row">
-        <div class="col-sm-12">
-          Table Data Pegawai
-        </div>
-      </div>
-    </div> --}}
     <div class="card-body">
       <div class="row">
         <div class="col-sm-12">
@@ -37,9 +29,8 @@
             <tr>
               <th style="text-align:center">No</th>
               <th style="text-align:center">Tgl</th>
-              <th style="text-align:center">Hasil Kinerja</th>
-              <th style="text-align:center">Foto</th>
-              <th style="text-align:center">PDF</th>
+              <th style="text-align:center">Rincian Kinerja</th>
+              <th style="text-align:center">Doc</th>
               <th style="text-align:center">Status</th>
               <th style="text-align:center">Aksi</th>
             </tr>
@@ -51,15 +42,37 @@
                 <td class="text-center">{{$no++}}</td>
                 <td class="text-center">{{$k->tgl}}</td>
                 <td class="text-center">{{$k->hasil}}</td>
-                <td class="text-center"><a href="{{ asset('template/dist/img/kinerja/'.$k['foto']) }}" class="btn btn-rounded btn-info style="border-radius:30px;""><i class="far fa-file-image"></i></a></td>
-                <td class="text-center"><a href="{{ asset('template/dist/img/kinerja/'.$k['doc']) }}" class="btn btn-rounded btn-info style="border-radius:30px;""><i class="far fa-file-pdf"></i></a></td>
-                <td class="text-center"><div class="badge {{ $k->status == "pending" ? "badge-warning" : "badge-success" }}">{{ $k->status }}</div></td>
-                <td>
-                <div class="d-grid gap-2 d-md-block" style="text-align:center">
-                  <a href="/edit-kinerja-pegawai/{{ $k->id }}" class="btn btn-warning"><i class="fas fa-edit"></i></a>
-                  <a href="/pegawai-hapus/{{ $k->id }}" class="btn btn-danger"><i class="fas fa-trash" onclick="return confirm('Yakin hapus data?')"></i></a>
-                </div>
-              </td>
+                <td class="text-center">
+                  <a href="{{ asset('template/dist/img/kinerja/'.$k['foto']) }}" class="btn btn-rounded btn-info" style="border-radius:30px;"><i class="far fa-file-image"></i></a>
+                  <a href="{{ asset('template/dist/img/kinerja/'.$k['doc']) }}" class="btn btn-rounded btn-info" style="border-radius:30px;"><i class="far fa-file-pdf"></i></a>
+                </td>
+                @if ($k->status == 'pending')
+                  <td class="text-center">
+                    <div class="badge {{ $k->status == "pending" ? "badge-warning" : "badge-success" }}">{{ $k->status }}</div></td>
+                  </td>
+                @else
+                  <td class="text-center">
+                    <span class="badge bg-danger">{{ $k->status }}</span>
+                  </td>
+                @endif  
+                @if ($k->status == 'draft')
+                  <td>
+                    <div class="d-grid gap-2 d-md-block" style="text-align:center">
+                      <a href="edit-draft/{{ $k->id }}" class="btn btn-warning"style="border-radius:30px;"><i class="fas fa-edit"></i></a>
+                      <a href="/pegawai/hapus/{{ $k->id }}" class="btn btn-danger" style="border-radius:30px;"><i class="fas fa-trash" onclick="return confirm('Yakin hapus data?')"></i></a>
+                      <a href="/restore/{{ $k->id }}" class="btn btn-success" style="border-radius:30px;" onclick="return confirm('PEHATIAN! Silahkan cek terlebih dahulu. Karena data yang disubmit tidak bisa diubah dan hapus')">Submit</a>
+                    </div>
+                  </td>
+                @else
+                  <td>
+                    <div class="d-grid gap-2 d-md-block" style="text-align:center">
+                      <div class="btn btn-warning disabled" style="border-radius:30px;"><i class="fas fa-edit"></i></div>
+                      <div class="btn btn-danger disabled" style="border-radius:30px;"><i class="fas fa-trash"></i></div>
+                      <div class="btn btn-success disabled" style="border-radius:30px;">Submit</div>
+                    </div>
+                  </td>
+                @endif
+                  
             </tr>
             @endforeach
           </tbody>
