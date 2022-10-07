d1ee4358ca423c16863337b19d7ee5b26c4f0d5f
balesta amanda <94122676+Projekaku@users.noreply.github.com>
2022-10-07 08:44:05 +0700

    Update LayoutController.php
    Signed-off-by: balesta amanda <94122676+Projekaku@users.noreply.github.com>

--- Controllers/LayoutController.php
+++ Controllers/LayoutController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Bidang;
+use App\Models\Kinerja;
+use Psy\Command\WhereamiCommand;
 
 class LayoutController extends Controller
 {
     public function index()
     {
         $user = Auth::User();
-        $pegawai = Auth::user()->id;
-        $hasilkinerja = Kinerja::where('angka', '0')->where('id_users',$pegawai)->count();
-        
+        $pegawai = Auth::User()->id;
+        $hasilkinerja = Kinerja::where('angka','0')->where('user_id',$pegawai)->count();
         $jml_pending = DB::table("kinerja")
             ->where("status", "pending")
+            // ->where("id", $user->bidang)
             ->count("status");
 
         $terverifikasi = DB::table("kinerja")
@@ -24,11 +29,16 @@ class LayoutController extends Controller
         $jml_dt_pegawai = User::count();
 
         $jml_bidang = Bidang::count();
-        
-        return view("layout.home",compact('hasilkinerja', "jml_pending",
+        return view(
+            "layout.home",
+            compact(
+                "jml_pending",
                 "jml_dt_pegawai",
                 "jml_bidang",
-                "terverifikasi"))->with([
+                "terverifikasi",
+                'hasilkinerja'
+            )
+        )->with([
             "user" => $user,
         ]);
     }
