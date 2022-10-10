249263a3163dfe29174784f47a51fc37217809be
Hardiana <87222054+hardiana4@users.noreply.github.com>
2022-10-10 10:20:55 +0700

    Update LaporanController.php
    Signed-off-by: Hardiana <87222054+hardiana4@users.noreply.github.com>

--- Controllers/LaporanController.php
+++ Controllers/LaporanController.php
@@ -10,4 +10,19 @@ class LaporanController extends Controller
     {
         echo "Laporan";
     }
+     public function laporan(Request $request)
+    {
+        $user = Auth::User();
+        $pegawai = Auth::user()->id;
+        $keyword = $request->keyword;
+        $kinerja = Kinerja::where('user_id',$pegawai)
+        ->where('status','=','success')
+        ->where('hasil','LIKE', '%'.$keyword.'%')->paginate();
+        $data = array
+        (
+            'kinerja' => $kinerja
+        );
+        return view('laporan_kinerja.lapkinerja_pgw',compact('kinerja'))->with([
+            "user" => $user,
+        ]);
 }
